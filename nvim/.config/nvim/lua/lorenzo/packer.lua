3b6514c76a7085b243de58e436cdbd95499d239f
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2022-12-28 18:20:18 +0100

    add nvim-surround

--- nvim/.config/nvim/lua/lorenzo/packer.lua
+++ nvim/.config/nvim/lua/lorenzo/packer.lua
@@ -77,4 +77,13 @@ return require('packer').startup(function(use)
         "iamcco/markdown-preview.nvim",
         run = function() vim.fn["mkdp#util#install"]() end,
     })
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)
