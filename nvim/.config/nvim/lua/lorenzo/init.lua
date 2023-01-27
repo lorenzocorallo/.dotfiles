c6c9d11744f409682f488cff68518eaa0fa4183e
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-01-28 00:41:21 +0100

    revert useless change

--- nvim/.config/nvim/lua/lorenzo/init.lua
+++ nvim/.config/nvim/lua/lorenzo/init.lua
@@ -1,12 +1,2 @@
 require("lorenzo.set")
 require("lorenzo.remap")
-
-function Color(color)
-
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
-end
-
-Color()
