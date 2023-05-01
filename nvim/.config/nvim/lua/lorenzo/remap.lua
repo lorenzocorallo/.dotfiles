412146293a78c0780c9230bcf62e5a5dec238213
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-05-01 16:19:22 +0200

    feat(nvim): add some keymaps

--- nvim/.config/nvim/lua/lorenzo/remap.lua
+++ nvim/.config/nvim/lua/lorenzo/remap.lua
@@ -4,12 +4,36 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
+-- move page up-down staying in the center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- search word staying in the center
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- switch to prev file 
+-- switch to prev file
 vim.keymap.set("n", "<C-b>", "<Cmd>e#<CR>")
 
+-- source curr file
+vim.keymap.set("n", "<leader><leader>", function()
+  vim.cmd("so")
+end)
+
+-- paste and delete don't lose yank
+vim.keymap.set("x", "p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "d", [["_d]])
+
+-- system clipboard copy-to
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Suffering
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- chmod +x curr file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
