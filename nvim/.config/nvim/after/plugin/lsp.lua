f4b1fcf4964d2dafb7e86f7e6bca9ea220e5880f
Lorenzo <lorenzo.corallo@outlook.it>
2023-06-10 17:35:15 +0200

    fix(nvim): set key for autocomplete confirm to <ENTER>

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -27,7 +27,7 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- use enter to confirm
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 lsp.setup_nvim_cmp({
