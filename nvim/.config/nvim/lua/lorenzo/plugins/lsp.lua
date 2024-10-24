3ae1332503aaed45dcfbb99fb8ce0a0b84ef2435
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2024-10-24 18:48:22 +0200

    fix(nvim): burn this shit :GoDoc remapping

--- nvim/.config/nvim/lua/lorenzo/plugins/lsp.lua
+++ nvim/.config/nvim/lua/lorenzo/plugins/lsp.lua
@@ -67,6 +67,7 @@ return {
     vim.g.go_fmt_autosave = 0
     vim.g.go_mod_fmt_autosave = 0
     vim.g.go_imports_autosave = 0
+    vim.g.go_doc_keywordprg_enabled = 0 -- fuck nnoremap K -> :GoDoc (i want my hover remap)
 
     vim.g.go_doc_popup_window = 1
     vim.g.go_term_enabled = 1
